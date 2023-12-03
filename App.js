438cc48cba495115b452e563770e8a0cf9a2be2e
wackopastorius <ladaegolaeva@gmail.com>
2023-12-03 19:41:05 +0100

    auth bros

--- App.js
+++ App.js
@@ -6,23 +6,30 @@ import VegRecipes from "./src/Components/VegRecipes";
 import MeatPoultryRecipes from "./src/Components/MeatPoultryRecipes"
 import SurfTurfRecipes from "./src/Components/SurfTurfRecipes";
 import SeafoodRecipes from "./src/Components/SeafoodRecipes";
+import SigninScreen from "./src/Components/SigninScreen";
+
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerTitle: "Breath of the Wild: Cookbook "}}>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="VegRecipes" component={VegRecipes} />
-        <Stack.Screen name="MeatPoultryRecipes" component={MeatPoultryRecipes} />
-        <Stack.Screen name="SurfTurfRecipes" component={SurfTurfRecipes} />
-        <Stack.Screen name="SeafoodRecipes" component={SeafoodRecipes} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerTitle: "Breath of the Wild: Cookbook "}}>
+          <Stack.Screen name="SigninScreen" component={SigninScreen}/>
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="VegRecipes" component={VegRecipes} />
+          <Stack.Screen name="MeatPoultryRecipes" component={MeatPoultryRecipes} />
+          <Stack.Screen name="SurfTurfRecipes" component={SurfTurfRecipes} />
+          <Stack.Screen name="SeafoodRecipes" component={SeafoodRecipes} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    
   );
 }
 
 const styles = StyleSheet.create({
   
 });
+
+
