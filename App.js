d4e07ad48fceef357f7358230e3e7d797681aa37
MichaelGranbäck <michael.granback@outlook.com>
2023-12-05 16:58:26 +0100

    Minns ej

--- App.js
+++ App.js
@@ -1,35 +1,37 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./src/Components/HomeScreen";
-import { StyleSheet} from "react-native";
-import VegRecipes from "./src/Components/VegRecipes";
-import MeatPoultryRecipes from "./src/Components/MeatPoultryRecipes"
-import SurfTurfRecipes from "./src/Components/SurfTurfRecipes";
-import SeafoodRecipes from "./src/Components/SeafoodRecipes";
-import SigninScreen from "./src/Components/SigninScreen";
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/Components/HomeScreen';
+import { StyleSheet } from 'react-native';
+import VegRecipes from './src/Components/VegRecipes';
+import MeatPoultryRecipes from './src/Components/MeatPoultryRecipes';
+import SurfTurfRecipes from './src/Components/SurfTurfRecipes';
+import SeafoodRecipes from './src/Components/SeafoodRecipes';
+import SigninScreen from './src/Components/SigninScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerTitle: "Breath of the Wild: Cookbook "}}>
-          <Stack.Screen name="SigninScreen" component={SigninScreen}/>
-          <Stack.Screen name="HomeScreen" component={HomeScreen} />
-          <Stack.Screen name="VegRecipes" component={VegRecipes} />
-          <Stack.Screen name="MeatPoultryRecipes" component={MeatPoultryRecipes} />
-          <Stack.Screen name="SurfTurfRecipes" component={SurfTurfRecipes} />
-          <Stack.Screen name="SeafoodRecipes" component={SeafoodRecipes} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerTitle: 'Breath of the Wild: Cookbook ' }}
+      >
+        <Stack.Screen name="SigninScreen" component={SigninScreen} />
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ headerLeft: false }}
+        />
+        <Stack.Screen name="VegRecipes" component={VegRecipes} />
+        <Stack.Screen
+          name="MeatPoultryRecipes"
+          component={MeatPoultryRecipes}
+        />
+        <Stack.Screen name="SurfTurfRecipes" component={SurfTurfRecipes} />
+        <Stack.Screen name="SeafoodRecipes" component={SeafoodRecipes} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  
-});
-
-
+const styles = StyleSheet.create({});
